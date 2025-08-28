f0d33c77ed3bd6c1e6f0f8d7971396a74127370a
AakashMaharjan929 <aakashmaharjan2222@gmail.com>
2025-08-28 09:41:17 +0545

    Completed Authentication

--- AI-Resume-Analyzer/app/routes/auth.tsx
+++ AI-Resume-Analyzer/app/routes/auth.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
 export const meta = () => {
   return [
     { title: "ResumeAI | Auth" },
@@ -5,7 +9,20 @@ export const meta = () => {
   ];
 };  
 
-const auth = () => {
+const Auth = () => {
+  const { isLoading, auth } = usePuterStore();
+
+  const location = useLocation();
+
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      navigate(next);
+    }
+  }, [auth.isAuthenticated, next]);
+
   return (
     <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center ">
         <div className="gradient-border shadow-lg">
@@ -14,10 +31,29 @@ const auth = () => {
                     <h1>Welcome</h1>
                     <h2>Log In to Continue your Job Journey</h2>
                 </div>
+                <div>
+                  {isLoading ? (
+                    <button className="auth-button animate-pulese">
+                      <p>Signing you in..</p>
+                    </button>
+                  ) : (
+                    <>
+                    {auth.isAuthenticated ? (
+                      <button className="auth-button" onClick={auth.signOut}>
+                        <p>Log Out</p>
+                      </button>
+                    ):(
+                      <button className="auth-button" onClick={auth.signIn}>
+                        <p>Log In</p>
+                      </button>
+                    )}
+                    </>
+                  )}
+                </div>
             </section>
         </div>
     </main>
   )
 }
 
-export default auth
+export default Auth
